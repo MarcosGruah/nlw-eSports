ea248c6f7dd414cc6ac26039321cc819b113ab7e
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2022-09-20 12:10:43 -0300

    Express routes and Prisma installation

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,30 @@
 import express from 'express';
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  return res.json([
+app.get('/games', (req, res) => {
+  return res.json([]);
+});
+
+app.post('/ads', (req, res) => {
+  return res.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (req, res) => {
+  // const gameId = req.params.id;
+  return res.status(201).json([
     { id: 1, name: 'Anúncio 1' },
     { id: 2, name: 'Anúncio 2' },
     { id: 3, name: 'Anúncio 3' },
     { id: 4, name: 'Anúncio 4' },
     { id: 5, name: 'Anúncio 5' },
   ]);
+});
+
+app.get('/ads/:id/discord', (req, res) => {
+  // const adId = req.params.id;
+  return res.json([]);
 });
 
 app.listen(port, () => {
